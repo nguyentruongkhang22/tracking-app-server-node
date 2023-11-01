f6e29944371f939ea44b29747ee3685b19b61cd6
Khang <nguyentruongkhang22@gmail.com>
2023-11-02 02:57:59 +0700

    restore websocket

--- common/websocket.ts
+++ common/websocket.ts
@@ -1,1 +1,13 @@
-import { wss } from "../server";
+import { WebSocketServer } from "ws";
+
+const wss: WebSocketServer = new WebSocketServer({
+  port: 3004,
+});
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    console.log(`Received message => ${message}`);
+    ws.send(`Hello, you sent => ${message}`);
+  });
+  ws.send("Hello, I am a server");
+});
--- server.ts
+++ server.ts
@@ -3,11 +3,11 @@ import swaggerUiExpress from "swagger-ui-express";
 import http from "http";
 
 import "./database/config";
+import "./common/websocket";
 import helmet from "helmet";
 import express from "express";
 import morgan from "morgan";
 import { indexRouter } from "./routers/index.router";
-import { WebSocketServer } from "ws";
 
 const app = express();
 const corsOptions = {
@@ -25,21 +25,4 @@ app.use("/docs", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerFile));
 
 server.listen(process.env.PORT || 3003, () => {
   console.log(`Server listening on port ${process.env.PORT || 3003}`);
-});
-
-/*
-  WebSocket
-*/
-const wss: WebSocketServer = new WebSocketServer({
-  server,
-  path: "/ws",
-  host: process.env.NODE_ENV == "dev" ? "127.0.0.1" : "api.lackadaisical.net",
-});
-
-wss.on("connection", (ws) => {
-  ws.on("message", (message) => {
-    console.log(`Received message => ${message}`);
-    ws.send(`Hello, you sent => ${message}`);
-  });
-  ws.send("Hello, I am a server");
 });
